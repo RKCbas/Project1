8593241f4b2e69324d1bb032ce66bdd3d9601f02
RKCbas <sebashp2015@hotmail.com>
2024-09-04 07:48:15 -0600

    added Modifiers to MainActivity.kt to show how to modify column properties

--- app/src/main/java/com/example/projecto1/MainActivity.kt
+++ app/src/main/java/com/example/projecto1/MainActivity.kt
@@ -4,16 +4,26 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.background
+import androidx.compose.foundation.border
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.example.projecto1.ui.theme.Projecto1Theme
 
 class MainActivity : ComponentActivity() {
@@ -21,25 +31,39 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
 //        enableEdgeToEdge()
         setContent {
-            //layouts
-            Column {
-                Text(text = "First Row")
-                Text(text = "Second Row")
-                Text(text = "Third Row")
-                Row {
-                    Text(text = "Text 1")
-                    Text(text = "Text 2")
-                    Text(text = "Text 3")
-                    Text(text = "Text 4")
-                    Text(text = "Text 5")
-                    Text(text = "Text 6")
-                }
-                Box{
-                    Text(text = "BoxText 1")
-                    Text(text = "BoxText 2")
-                }
-                Greeting(name = "World")
+            Column (
+                modifier = Modifier.fillMaxSize(),
+                verticalArrangement = Arrangement.Center,
+                horizontalAlignment = androidx.compose.ui.Alignment.CenterHorizontally
+            ){
+                Text(text = "Hola Android!")
+                ModifierExample()
+                ModifierExample2()
+                ModifierExample3()
             }
+
+
+
+
+            //layouts
+//            Column {
+//                Text(text = "First Row")
+//                Text(text = "Second Row")
+//                Text(text = "Third Row")
+//                Row {
+//                    Text(text = "Text 1")
+//                    Text(text = "Text 2")
+//                    Text(text = "Text 3")
+//                    Text(text = "Text 4")
+//                    Text(text = "Text 5")
+//                    Text(text = "Text 6")
+//                }
+//                Box{
+//                    Text(text = "BoxText 1")
+//                    Text(text = "BoxText 2")
+//                }
+//                Greeting(name = "World")
+//            }
 //test
 
 //            Projecto1Theme {
@@ -68,4 +92,57 @@ fun GreetingPreview() {
     Projecto1Theme {
         Greeting("Sebastián")
     }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun ModifierExample(){
+    Column(
+        modifier = Modifier
+            .padding(24.dp)
+            .clickable(onClick = { clickAction() })
+    ) {
+        Text(text = "Hola Android!")
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun ModifierExample2(){
+    Column(
+        modifier = Modifier
+            .padding(24.dp)
+            .fillMaxWidth()
+            .clickable(onClick = { clickAction() })
+    ) {
+        Text(text = "Hola Android!")
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun ModifierExample3(){
+    Column(
+        modifier = Modifier
+            .fillMaxHeight()
+            .clickable(onClick = { clickAction() })
+            .padding(16.dp)
+            .background(Color.Cyan)
+            .border(width = 2.dp, Color.Green)
+            .width(200.dp),
+            horizontalAlignment = Alignment.CenterHorizontally,
+            verticalArrangement = Arrangement.SpaceEvenly
+    ) {
+        Text(text = "Item 1")
+        Text(text = "Item 2")
+        Text(text = "Item 3")
+        Text(text = "Item 4")
+        Text(text = "Item 5")
+        Text(text = "Item 6")
+    }
+}
+
+
+fun clickAction(element:String = "Elemento") {
+    println("$element Clicked")
 }
