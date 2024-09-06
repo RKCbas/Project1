16cfbc70b61406c264756207b0fd92a4116763b2
RKCbas <sebashp2015@hotmail.com>
2024-09-06 07:44:20 -0600

    Added custom text implementation and image implementation

--- app/src/main/java/com/example/projecto1/MainActivity.kt
+++ app/src/main/java/com/example/projecto1/MainActivity.kt
@@ -1,9 +1,11 @@
 package com.example.projecto1
 
+import android.media.Image
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.border
 import androidx.compose.foundation.clickable
@@ -21,9 +23,21 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Brush
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.Color.Companion.Blue
+import androidx.compose.ui.graphics.Color.Companion.Cyan
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.colorResource
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.semantics.Role.Companion.Image
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.FontStyle
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.example.projecto1.ui.theme.Projecto1Theme
 
 class MainActivity : ComponentActivity() {
@@ -36,10 +50,12 @@ class MainActivity : ComponentActivity() {
                 verticalArrangement = Arrangement.Center,
                 horizontalAlignment = androidx.compose.ui.Alignment.CenterHorizontally
             ){
-                Text(text = "Hola Android!")
-                ModifierExample()
-                ModifierExample2()
-                ModifierExample3()
+//                Text(text = "Hola Android!")
+//                ModifierExample()
+//                ModifierExample2()
+//                ModifierExample3()
+                CustomText()
+                Picture()
             }
 
 
@@ -86,7 +102,7 @@ fun Greeting(name: String, modifier: Modifier = Modifier) {
     )
 }
 
-@Preview(showBackground = true)
+//@Preview(showBackground = true)
 @Composable
 fun GreetingPreview() {
     Projecto1Theme {
@@ -94,7 +110,7 @@ fun GreetingPreview() {
     }
 }
 
-@Preview(showBackground = true)
+//@Preview(showBackground = true)
 @Composable
 fun ModifierExample(){
     Column(
@@ -106,7 +122,7 @@ fun ModifierExample(){
     }
 }
 
-@Preview(showBackground = true)
+//@Preview(showBackground = true)
 @Composable
 fun ModifierExample2(){
     Column(
@@ -119,7 +135,7 @@ fun ModifierExample2(){
     }
 }
 
-@Preview(showBackground = true)
+//@Preview(showBackground = true)
 @Composable
 fun ModifierExample3(){
     Column(
@@ -139,6 +155,43 @@ fun ModifierExample3(){
         Text(text = "Item 4")
         Text(text = "Item 5")
         Text(text = "Item 6")
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun CustomText(){
+    Column {
+        Text(
+            stringResource(R.string.hello_world_text),
+            color = colorResource(R.color.purple_200),
+            fontSize = 28.sp,
+            fontStyle = FontStyle.Italic,
+            fontWeight = FontWeight.ExtraBold
+        )
+        val gradientColors = listOf(Cyan, Blue)
+        Text(
+            stringResource(R.string.hello_world_text),
+            style = TextStyle(brush = Brush.linearGradient(colors = gradientColors))
+        )
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun Picture(){
+    Column (
+        modifier = Modifier
+            .fillMaxWidth()
+            .background(Color.Black)
+    ){
+        Image(
+            modifier = Modifier
+                .fillMaxWidth(),
+            painter = painterResource(R.drawable.andcat),
+            contentDescription = "Logo Andorid",
+            contentScale = ContentScale.Crop
+        )
     }
 }
 
