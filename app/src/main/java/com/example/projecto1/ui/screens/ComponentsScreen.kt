646e40caec40758785302798ef1d7b9d3b41f6ab
RKCbas <sebashp2015@hotmail.com>
2024-10-02 09:34:21 -0600

    Added 2 items to the modal drawer sheet that includes time and date pickers, for the time pickers added 2 examples and for the date pickers added a box that displays the date picker and to check the selected date added a function that convert milliseconds to a string formated

--- app/src/main/java/com/example/projecto1/ui/screens/ComponentsScreen.kt
+++ app/src/main/java/com/example/projecto1/ui/screens/ComponentsScreen.kt
@@ -1,9 +1,14 @@
 package com.example.projecto1.ui.screens
 
+import android.widget.DatePicker
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.offset
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
@@ -12,34 +17,48 @@ import androidx.compose.material.icons.filled.AccountBox
 import androidx.compose.material.icons.filled.Add
 import androidx.compose.material.icons.filled.Check
 import androidx.compose.material.icons.filled.Close
+import androidx.compose.material.icons.filled.DateRange
 import androidx.compose.material.icons.filled.Person
+import androidx.compose.material.icons.filled.ShoppingCart
 import androidx.compose.material3.AssistChip
 import androidx.compose.material3.AssistChipDefaults
+import androidx.compose.material3.Badge
+import androidx.compose.material3.BadgedBox
 import androidx.compose.material3.Button
 import androidx.compose.material3.Checkbox
 import androidx.compose.material3.CircularProgressIndicator
+import androidx.compose.material3.DatePicker
+import androidx.compose.material3.DatePickerDialog
 import androidx.compose.material3.DrawerValue
 import androidx.compose.material3.ElevatedButton
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.ExtendedFloatingActionButton
 import androidx.compose.material3.FilledTonalButton
 import androidx.compose.material3.FilterChip
 import androidx.compose.material3.FloatingActionButton
 import androidx.compose.material3.HorizontalDivider
 import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
 import androidx.compose.material3.InputChip
 import androidx.compose.material3.InputChipDefaults
 import androidx.compose.material3.LargeFloatingActionButton
 import androidx.compose.material3.LinearProgressIndicator
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.ModalDrawerSheet
 import androidx.compose.material3.ModalNavigationDrawer
 import androidx.compose.material3.NavigationDrawerItem
 import androidx.compose.material3.OutlinedButton
+import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Slider
 import androidx.compose.material3.SmallFloatingActionButton
 import androidx.compose.material3.Switch
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextButton
+import androidx.compose.material3.TimeInput
+import androidx.compose.material3.TimePicker
+import androidx.compose.material3.rememberDatePickerState
 import androidx.compose.material3.rememberDrawerState
+import androidx.compose.material3.rememberTimePickerState
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
@@ -48,11 +67,18 @@ import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.shadow
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.window.Popup
 import androidx.navigation.NavController
 import kotlinx.coroutines.launch
+import java.text.SimpleDateFormat
+import java.util.Calendar
+import java.util.Date
+import java.util.Locale
 
 @Composable
 fun ComponentsScreen(navController: NavController) {
@@ -170,6 +196,45 @@ fun ComponentsScreen(navController: NavController) {
                         }
                     }
                 )
+                NavigationDrawerItem(
+                    label = { Text(text = "Badges") },
+                    selected = false,
+                    onClick = {
+                        component = "Badges"
+                        scope.launch {
+//                            drawerState.apply {
+//                                close()
+//                            }
+                            drawerState.close()
+                        }
+                    }
+                )
+                NavigationDrawerItem(
+                    label = { Text(text = "Time Pickers") },
+                    selected = false,
+                    onClick = {
+                        component = "TimePickers"
+                        scope.launch {
+//                            drawerState.apply {
+//                                close()
+//                            }
+                            drawerState.close()
+                        }
+                    }
+                )
+                NavigationDrawerItem(
+                    label = { Text(text = "Date Pickers") },
+                    selected = false,
+                    onClick = {
+                        component = "DatePickers"
+                        scope.launch {
+//                            drawerState.apply {
+//                                close()
+//                            }
+                            drawerState.close()
+                        }
+                    }
+                )
             }
         }
     ) {
@@ -206,6 +271,18 @@ fun ComponentsScreen(navController: NavController) {
 
                 "Switches" -> {
                     Switches()
+                }
+
+                "Badges" -> {
+                    Badges()
+                }
+
+                "TimePickers" -> {
+                    TimePickers()
+                }
+
+                "DatePickers" -> {
+                    DatePickers()
                 }
             }
         }
@@ -322,10 +399,10 @@ fun Chips() {
 }
 
 @Composable
-fun  InputChipExample(
-    text:String,
+fun InputChipExample(
+    text: String,
     onDismiss: () -> Unit
-){
+) {
     var enable by remember { mutableStateOf(true) }
     if (!enable) return
 
@@ -383,7 +460,7 @@ fun Sliders() {
         Column {
             Slider(
                 value = sliderPosition,
-                onValueChange = {sliderPosition = it},
+                onValueChange = { sliderPosition = it },
                 steps = 9,
                 valueRange = 0f..100f
             )
@@ -406,13 +483,13 @@ fun Switches() {
         modifier = Modifier.fillMaxSize()
     ) {
         var checked by remember { mutableStateOf(true) }
-        Switch(checked = checked, onCheckedChange = {checked = it})
+        Switch(checked = checked, onCheckedChange = { checked = it })
 
         var checked2 by remember { mutableStateOf(true) }
         Switch(
             checked = checked2,
-            onCheckedChange = {checked2 = it},
-            thumbContent = if (checked2){
+            onCheckedChange = { checked2 = it },
+            thumbContent = if (checked2) {
                 {
                     Icon(
                         Icons.Filled.Check,
@@ -420,14 +497,191 @@ fun Switches() {
                         Modifier.size(InputChipDefaults.AvatarSize)
                     )
                 }
-            }else{
+            } else {
                 null
             }
 
         )
 
         var checked3 by remember { mutableStateOf(true) }
-        Checkbox(checked = checked3, onCheckedChange = {checked3 = it})
+        Checkbox(checked = checked3, onCheckedChange = { checked3 = it })
 
     }
+}
+
+//@Preview(showBackground = true)
+@Composable
+fun Badges() {
+    Column(
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.SpaceEvenly,
+        modifier = Modifier.fillMaxSize()
+    ) {
+        var itemCount by remember { mutableStateOf(0) }
+
+        BadgedBox(
+            badge = {
+                if (itemCount > 0) {
+                    Badge(
+                        containerColor = Color.Red,
+                        contentColor = Color.White
+                    ) {
+                        Text(text = "$itemCount")
+                    }
+                }
+            }
+        ) {
+            Icon(imageVector = Icons.Filled.ShoppingCart, contentDescription = "Carrito")
+        }
+
+        Button(onClick = { itemCount++ }) {
+            Text(text = "Add Item")
+        }
+
+    }
+}
+
+//@Preview(showBackground = true)
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun TimePickers() {
+
+    Column(
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.SpaceEvenly,
+        modifier = Modifier.fillMaxSize()
+    ) {
+        InputExample(onConfirm = { /*TODO*/ }){}
+        DialExample(onConfirm = { /*TODO*/ }) {
+
+        }
+    }
+}
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun InputExample(
+    onConfirm: () -> Unit,
+    onDismiss: () -> Unit,
+) {
+    val currentTime = Calendar.getInstance()
+
+    val timePickerState = rememberTimePickerState(
+        initialHour = currentTime.get(Calendar.HOUR_OF_DAY),
+        initialMinute = currentTime.get(Calendar.MINUTE),
+        is24Hour = true,
+    )
+
+    Column {
+        TimeInput(
+            state = timePickerState,
+        )
+        Button(onClick = onDismiss) {
+            Text("Dismiss picker")
+        }
+        Button(onClick = onConfirm) {
+            Text("Confirm selection")
+        }
+    }
+}
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun DialExample(
+    onConfirm: () -> Unit,
+    onDismiss: () -> Unit,
+) {
+    val currentTime = Calendar.getInstance()
+
+    val timePickerState = rememberTimePickerState(
+        initialHour = currentTime.get(Calendar.HOUR_OF_DAY),
+        initialMinute = currentTime.get(Calendar.MINUTE),
+        is24Hour = true,
+    )
+
+    Column {
+        TimePicker(
+            state = timePickerState,
+        )
+        Button(onClick = onDismiss) {
+            Text("Dismiss picker")
+        }
+        Button(onClick = onConfirm) {
+            Text("Confirm selection")
+        }
+    }
+}
+
+
+
+@Preview(showBackground = true)
+@Composable
+fun DatePickers(){
+
+    Column(
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.SpaceEvenly,
+        modifier = Modifier.fillMaxSize()
+    ){
+        DatePickerDocked()
+
+    }
+}
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun DatePickerDocked() {
+    var showDatePicker by remember { mutableStateOf(false) }
+    val datePickerState = rememberDatePickerState()
+    val selectedDate = datePickerState.selectedDateMillis?.let {
+        convertMillisToDate(it)
+    } ?: ""
+
+    Box(
+        modifier = Modifier.fillMaxWidth()
+    ) {
+        OutlinedTextField(
+            value = selectedDate,
+            onValueChange = { },
+            label = { Text("DOB") },
+            readOnly = true,
+            trailingIcon = {
+                IconButton(onClick = { showDatePicker = !showDatePicker }) {
+                    Icon(
+                        imageVector = Icons.Default.DateRange,
+                        contentDescription = "Select date"
+                    )
+                }
+            },
+            modifier = Modifier
+                .fillMaxWidth()
+                .height(64.dp)
+        )
+
+        if (showDatePicker) {
+            Popup(
+                onDismissRequest = { showDatePicker = false },
+                alignment = Alignment.TopStart
+            ) {
+                Box(
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .offset(y = 64.dp)
+                        .shadow(elevation = 4.dp)
+                        .background(MaterialTheme.colorScheme.surface)
+                        .padding(16.dp)
+                ) {
+                    DatePicker(
+                        state = datePickerState,
+                        showModeToggle = false
+                    )
+                }
+            }
+        }
+    }
+}
+
+fun convertMillisToDate(millis: Long): String {
+    val formatter = SimpleDateFormat("MM/dd/yyyy", Locale.getDefault())
+    return formatter.format(Date(millis))
 }
