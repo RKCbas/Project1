609ee9a5d3a601279d733e01a36bb913f54db75d
RKCbas <sebashp2015@hotmail.com>
2024-10-03 07:52:18 -0600

    Added SnackBars, AlertDialogs and Bars functions to ComponentsScreen.kt

--- app/src/main/java/com/example/projecto1/MainActivity.kt
+++ app/src/main/java/com/example/projecto1/MainActivity.kt
@@ -64,7 +64,7 @@ import com.example.projecto1.ui.screens.MenuScreen
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-//        enableEdgeToEdge()
+        enableEdgeToEdge()
         setContent {
             ComposeMultiScreenApp()
 
--- app/src/main/java/com/example/projecto1/ui/screens/ComponentsScreen.kt
+++ app/src/main/java/com/example/projecto1/ui/screens/ComponentsScreen.kt
@@ -1,10 +1,12 @@
 package com.example.projecto1.ui.screens
 
+import android.app.AlertDialog
 import android.widget.DatePicker
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
@@ -18,8 +20,12 @@ import androidx.compose.material.icons.filled.Add
 import androidx.compose.material.icons.filled.Check
 import androidx.compose.material.icons.filled.Close
 import androidx.compose.material.icons.filled.DateRange
+import androidx.compose.material.icons.filled.Menu
 import androidx.compose.material.icons.filled.Person
+import androidx.compose.material.icons.filled.Settings
 import androidx.compose.material.icons.filled.ShoppingCart
+import androidx.compose.material.icons.filled.Warning
+import androidx.compose.material3.AlertDialog
 import androidx.compose.material3.AssistChip
 import androidx.compose.material3.AssistChipDefaults
 import androidx.compose.material3.Badge
@@ -51,6 +57,8 @@ import androidx.compose.material3.OutlinedButton
 import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Slider
 import androidx.compose.material3.SmallFloatingActionButton
+import androidx.compose.material3.SnackbarHost
+import androidx.compose.material3.SnackbarHostState
 import androidx.compose.material3.Switch
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextButton
@@ -69,9 +77,11 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.shadow
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import androidx.compose.ui.window.Popup
 import androidx.navigation.NavController
 import kotlinx.coroutines.launch
@@ -235,6 +245,45 @@ fun ComponentsScreen(navController: NavController) {
                         }
                     }
                 )
+                NavigationDrawerItem(
+                    label = { Text(text = "SnackBar") },
+                    selected = false,
+                    onClick = {
+                        component = "SnackBar"
+                        scope.launch {
+//                            drawerState.apply {
+//                                close()
+//                            }
+                            drawerState.close()
+                        }
+                    }
+                )
+                NavigationDrawerItem(
+                    label = { Text(text = "Alert Dialog") },
+                    selected = false,
+                    onClick = {
+                        component = "AlertDialog"
+                        scope.launch {
+//                            drawerState.apply {
+//                                close()
+//                            }
+                            drawerState.close()
+                        }
+                    }
+                )
+                NavigationDrawerItem(
+                    label = { Text(text = "Bars") },
+                    selected = false,
+                    onClick = {
+                        component = "Bars"
+                        scope.launch {
+//                            drawerState.apply {
+//                                close()
+//                            }
+                            drawerState.close()
+                        }
+                    }
+                )
             }
         }
     ) {
@@ -283,6 +332,15 @@ fun ComponentsScreen(navController: NavController) {
 
                 "DatePickers" -> {
                     DatePickers()
+                }
+                "SnackBar" -> {
+                    SnackBars()
+                }
+                "AlertDialog" -> {
+                    AlertDialogs()
+                }
+                "Bars" -> {
+                    Bars()
                 }
             }
         }
@@ -551,7 +609,7 @@ fun TimePickers() {
         verticalArrangement = Arrangement.SpaceEvenly,
         modifier = Modifier.fillMaxSize()
     ) {
-        InputExample(onConfirm = { /*TODO*/ }){}
+        InputExample(onConfirm = { /*TODO*/ }) {}
         DialExample(onConfirm = { /*TODO*/ }) {
 
         }
@@ -613,16 +671,15 @@ fun DialExample(
 }
 
 
-
-@Preview(showBackground = true)
+//@Preview(showBackground = true)
 @Composable
-fun DatePickers(){
+fun DatePickers() {
 
     Column(
         horizontalAlignment = Alignment.CenterHorizontally,
         verticalArrangement = Arrangement.SpaceEvenly,
         modifier = Modifier.fillMaxSize()
-    ){
+    ) {
         DatePickerDocked()
 
     }
@@ -684,4 +741,108 @@ fun DatePickerDocked() {
 fun convertMillisToDate(millis: Long): String {
     val formatter = SimpleDateFormat("MM/dd/yyyy", Locale.getDefault())
     return formatter.format(Date(millis))
+}
+
+//@Preview(showBackground = true)
+@Composable
+fun SnackBars(){
+    Column(
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.SpaceEvenly,
+        modifier = Modifier.fillMaxSize()
+    ) {
+        var snackState = remember { SnackbarHostState() }
+        var snackScope = rememberCoroutineScope()
+
+        SnackbarHost(hostState = snackState, Modifier)
+
+        fun launchSnackBar(){
+            snackScope.launch { snackState.showSnackbar("The message was sent") }
+        }
+
+        Button(::launchSnackBar) {
+            Text(text = "Show SnackBar")
+        }
+//        Button(onClick = { launchSnackBar() }) {
+//            Text(text = "Show SnackBar")
+//        }
+
+    }
+}
+
+//@Preview(showBackground = true)
+@Composable
+fun AlertDialogs() {
+    Column(
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.SpaceEvenly,
+        modifier = Modifier.fillMaxSize()
+    ) {
+        var showAlertDialog by remember { mutableStateOf(false) }
+        var selectedOption by remember { mutableStateOf("") }
+
+
+        if (showAlertDialog){
+            AlertDialog(
+                icon = { Icon(Icons.Filled.Warning, contentDescription = "") },
+                title = { Text(text = "Confirm deletion") },
+                text = { Text(text = "Are you sure you want to delete the file?") },
+                onDismissRequest = {/*TO DO*/ },
+                confirmButton = {
+                    TextButton(
+                        onClick = {
+                            selectedOption = "Confirm"
+                            showAlertDialog = false
+                        }
+                    ) {
+                        Text(text = "Confirmar")
+                    }
+                },
+                dismissButton = {
+                    TextButton(
+                        onClick = {
+                            selectedOption = "Dismiss"
+                            showAlertDialog = false
+                        }
+                    ) {
+                        Text(text = "Dismiss")
+                    }
+                }
+            )
+        }
+
+        Text(selectedOption)
+        Button(onClick = { showAlertDialog = true }) {
+            Text(text = "Show alert dialog")
+        }
+
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun Bars() {
+    Box(
+        modifier = Modifier
+            .fillMaxSize()
+            .background(Color.DarkGray)
+    ){
+        Row (
+            modifier = Modifier
+                .align(Alignment.TopCenter)
+                .fillMaxWidth()
+                .background(Color.Black)
+                .padding(10.dp, 50.dp, 10.dp, 10.dp),
+            horizontalArrangement = Arrangement.SpaceBetween
+        ){
+            Icon(Icons.Filled.Menu, contentDescription = "", tint = Color.White)
+            Text(
+                text = "App Title",
+                color = Color.White,
+                fontWeight = FontWeight.Bold,
+                fontSize = 20.sp
+            )
+            Icon(Icons.Filled.Settings, contentDescription = "", tint = Color.White)
+        }
+    }
 }
