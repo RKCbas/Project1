319ff9fc02287fd591a971ff6f0fcd2539be68a7
RKCbas <sebashp2015@hotmail.com>
2024-10-04 07:58:14 -0600

    Added a row at the bottom of Bars function to display several columns with an icon button and a text to make an navbar and also added in the middle a column that displays several texts with spacers and divider using a function called posts

---
+++ app/src/main/java/com/example/projecto1/data/model/PostModel.kt
@@ -0,0 +1,8 @@
+package com.example.projecto1.data.model
+
+data class PostModel(
+    val id:Int,
+    val title:String,
+    val text:String,
+)
+
--- app/src/main/java/com/example/projecto1/ui/screens/ComponentsScreen.kt
+++ app/src/main/java/com/example/projecto1/ui/screens/ComponentsScreen.kt
@@ -2,11 +2,15 @@ package com.example.projecto1.ui.screens
 
 import android.app.AlertDialog
 import android.widget.DatePicker
+import androidx.annotation.StringRes
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
+//import androidx.compose.foundation.layout.BoxScopeInstance.align
 import androidx.compose.foundation.layout.Column
+//import androidx.compose.foundation.layout.FlowRowScopeInstance.align
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
@@ -14,6 +18,10 @@ import androidx.compose.foundation.layout.offset
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.AccountBox
 import androidx.compose.material.icons.filled.Add
@@ -25,6 +33,11 @@ import androidx.compose.material.icons.filled.Person
 import androidx.compose.material.icons.filled.Settings
 import androidx.compose.material.icons.filled.ShoppingCart
 import androidx.compose.material.icons.filled.Warning
+import androidx.compose.material.icons.outlined.Email
+import androidx.compose.material.icons.outlined.Favorite
+import androidx.compose.material.icons.outlined.Home
+import androidx.compose.material.icons.outlined.Search
+import androidx.compose.material.icons.outlined.Settings
 import androidx.compose.material3.AlertDialog
 import androidx.compose.material3.AssistChip
 import androidx.compose.material3.AssistChipDefaults
@@ -77,6 +90,7 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.shadow
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
@@ -84,6 +98,8 @@ import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.compose.ui.window.Popup
 import androidx.navigation.NavController
+import com.example.projecto1.R
+import com.example.projecto1.data.model.PostModel
 import kotlinx.coroutines.launch
 import java.text.SimpleDateFormat
 import java.util.Calendar
@@ -843,6 +859,96 @@ fun Bars() {
                 fontSize = 20.sp
             )
             Icon(Icons.Filled.Settings, contentDescription = "", tint = Color.White)
+        }
+        var post = arrayOf(
+            PostModel(1,"Title 1", "Text 1"),
+            PostModel(2,"Title 2", "Text 2"),
+            PostModel(3,"Title 3", "Text 3"),
+            PostModel(4,"Title 4", "Text 4"),
+            PostModel(5,"Title 5", "Text 5")
+        )
+        Column(
+            modifier = Modifier
+                .align(Alignment.TopCenter)
+                .padding(10.dp, 90.dp, 10.dp, 10.dp)
+                .fillMaxSize()
+        ) {
+            Posts(arrayPosts = post)
+        }
+        Row (
+            modifier= Modifier
+                .align(Alignment.BottomCenter)
+                .fillMaxWidth()
+                .background(Color.Black)
+                .padding(2.dp, 5.dp),
+            horizontalArrangement = Arrangement.SpaceEvenly
+        ){
+            Column {
+                IconButton(onClick = {}, modifier=Modifier.size(30.dp)) {
+                    Icon(
+                        Icons.Outlined.Home,
+                        contentDescription = "",
+                        tint = Color.White,
+                        modifier = Modifier
+                            .fillMaxSize()
+                    )
+                }
+                Text(text ="Home", color = Color.White)
+            }
+            Column {
+                IconButton(onClick = {}, modifier=Modifier.size(30.dp)) {
+                    Icon(
+                        Icons.Outlined.Email,
+                        contentDescription = "",
+                        tint = Color.White,
+                        modifier = Modifier
+                            .fillMaxSize()
+                    )
+                }
+                Text(text ="Email", color = Color.White)
+            }
+            Column {
+                IconButton(onClick = {}, modifier=Modifier.size(30.dp)) {
+                    Icon(
+                        Icons.Outlined.Favorite,
+                        contentDescription = "",
+                        tint = Color.White,
+                        modifier = Modifier
+                            .fillMaxSize()
+                    )
+                }
+                Text(text ="Liked", color = Color.White)
+            }
+            Column {
+                IconButton(onClick = {}, modifier=Modifier.size(30.dp)) {
+                    Icon(
+                        Icons.Outlined.Settings,
+                        contentDescription = "",
+                        tint = Color.White,
+                        modifier = Modifier
+                            .fillMaxSize()
+                    )
+                }
+                Text(text ="Settings", color = Color.White)
+            }
+        }
+    }
+}
+
+@Composable
+fun Posts(arrayPosts:Array<PostModel>) {
+    LazyColumn(
+        modifier = Modifier
+            .fillMaxSize()
+    ){
+        items(arrayPosts){ post->
+            Text(
+                text = post.text,
+                color = Color.White,
+                fontSize = 16.sp
+            )
+            Spacer(modifier = Modifier.height(16.dp))
+            HorizontalDivider(thickness = 2.dp)
         }
     }
 }
